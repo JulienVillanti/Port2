b1dc44814edd21eced965674f5714ff3bdc80efd
Julien <villantijulien@gmail.com>
2024-03-12 19:33:45 -0400

    fixed navbar for good

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
+// Hamburger menu part===========================================
+
+const burgerMenu = document.getElementById('burger-menu');
+const navList = document.querySelector('.nav-list');
+
+burgerMenu.addEventListener('click', () => {
+    navList.classList.toggle('active');
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Select all navigation links
+    const navLinks = document.querySelectorAll(".nav-link");
+
+    // Add click event listener to each navigation link
+    navLinks.forEach(function (navLink) {
+        navLink.addEventListener("click", function (event) {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Get the target section ID from the href attribute
+            const targetId = this.getAttribute("href").substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            // Scroll smoothly to the target section
+            targetSection.scrollIntoView({
+                behavior: "smooth"
+            });
+
+            // Close the navigation menu if it's open
+            const navList = document.querySelector(".nav-list");
+            navList.classList.remove("active");
+
+            // Toggle the menu icon
+            const menuIcon = document.querySelector("#burger-menu i");
+            menuIcon.classList.remove("fa-times");
+            menuIcon.classList.add("fa-bars");
+        });
+    });
+});
+
+
+//scroll btn part=============================================
 let scrollBtn = document.getElementById('scroll-up');
 let scrollIconBtn = document.getElementById('scroll-up-icon');
 
@@ -40,6 +82,10 @@ function topFunctionIcon() {
 
 toggleButtons();
 
+// form validation ======================================
 
-// Hamburger menu part
-
+function validate() {
+    let name = document.getElementById('name-field').value;
+    let email = document.getElementById('email-field').value;
+    let message = document.getElementById('message-field').value;
+}
