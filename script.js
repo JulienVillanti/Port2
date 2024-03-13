79490d80b781358524358902009281cde820f102
Julien <villantijulien@gmail.com>
2024-03-12 23:16:21 -0400

    mostly done, re-test before submission tomorrow

--- script.js
+++ script.js
@@ -8,29 +8,29 @@ burgerMenu.addEventListener('click', () => {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-    // Select all navigation links
+
     const navLinks = document.querySelectorAll(".nav-link");
 
-    // Add click event listener to each navigation link
+
     navLinks.forEach(function (navLink) {
         navLink.addEventListener("click", function (event) {
-            // Prevent default anchor click behavior
+
             event.preventDefault();
 
-            // Get the target section ID from the href attribute
+
             const targetId = this.getAttribute("href").substring(1);
             const targetSection = document.getElementById(targetId);
 
-            // Scroll smoothly to the target section
+
             targetSection.scrollIntoView({
                 behavior: "smooth"
             });
 
-            // Close the navigation menu if it's open
+
             const navList = document.querySelector(".nav-list");
             navList.classList.remove("active");
 
-            // Toggle the menu icon
+
             const menuIcon = document.querySelector("#burger-menu i");
             menuIcon.classList.remove("fa-times");
             menuIcon.classList.add("fa-bars");
@@ -83,9 +83,43 @@ function topFunctionIcon() {
 toggleButtons();
 
 // form validation ======================================
-
-function validate() {
+// Test form gathering
+function validateAndSubmit() {
     let name = document.getElementById('name-field').value;
     let email = document.getElementById('email-field').value;
     let message = document.getElementById('message-field').value;
+
+    if (!isValidEmail(email)) {
+        alert('Please enter a valid email!')
+        return;
+    }
+
+    const formData = "Name: " + name + "\n" +
+        "Email: " + email + "\n" +
+        "Message: " + message + "\n\n";
+
+
+    const blob = new Blob([formData], { type: "text/plain;charset=utf-8" });
+
+    let a = document.createElement('a');
+    a.style.display = 'none';
+    document.body.appendChild(a);
+
+    a.download = "form_data.txt";
+
+    a.href = window.URL.createObjectURL(blob);
+
+    a.click();
+
+    window.URL.revokeObjectURL(a.href);
+    document.body.removeChild(a);
 }
+
+function isValidEmail(email) {
+    let emailRgx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;;
+    return emailRgx.test(email);
+}
+
+//NOTE===========================
+//unfortunately could not get the form info to be forwarded to me, it downloads the info to the senders computer as a .txt file, 
+//but cannot formward it to me, I could've user a 3rd party platform but will probably update once we studied DB.
